125b2ed6c97cfe21a0925c5528ced7fdc1b73a22
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-05-27 20:21:50 -0400

    Removed the api key for now, will need to add it back in though as I don't think there is a way to make use of a .env type file for android, nor do I think it makes sense to do so

--- kotlin/app/src/main/java/com/hatchways/loginidsdksample/MyApplication.kt
+++ kotlin/app/src/main/java/com/hatchways/loginidsdksample/MyApplication.kt
@@ -8,8 +8,9 @@ class MyApplication : Application() {
     // Overriding this method is totally optional!
     override fun onCreate() {
         super.onCreate()
-        val clientId = "IYFdqXZYo2ryFXNxDFCLrEqONWOTk-QrxVOIzvwJ_D_iJxRMlmUCBiXXGxPthFBP4B906jZCnXQ7TfcF7ykveQ=="
-        val baseUrl = "https://a4a49f60-9edb-11ea-b43f-93aac17785e0.sandbox.native-api.auth.asliri.id"
+        System.getenv()
+        val clientId = System.getenv("LOGINIDCLIENTID")
+        val baseUrl = System.getProperty("LOGINIDBASEURL")
         LoginApi.client().configure(this, clientId, baseUrl)
         // Required initialization logic here!
     }

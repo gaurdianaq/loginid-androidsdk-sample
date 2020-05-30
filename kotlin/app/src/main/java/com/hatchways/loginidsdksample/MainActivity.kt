ec2edd80cdaecb6e583fcff7eca3699085224bc3
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-05-30 16:54:19 -0400

    Some basic form of an app exists now

--- kotlin/app/src/main/java/com/hatchways/loginidsdksample/MainActivity.kt
+++ kotlin/app/src/main/java/com/hatchways/loginidsdksample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import login.api.LoginApi
 import login.api.LoginCallback
@@ -19,18 +20,36 @@ class MainActivity : AuthActivity() {
 
     override fun onResume() {
         super.onResume()
+        if (LoginApi.client().hasAccount()) {
+            deleteBtn.visibility = View.VISIBLE
+        }
+        else {
+            deleteBtn.visibility = View.INVISIBLE
+        }
         if (LoginApi.client().isLoggedIn) {
             supportFragmentManager.beginTransaction().replace(R.id.frameLayout, AuthenticatedFragment()).commit()
-            authenticate_btn.text = getString(R.string.authenticate_button_message)
+            authenticate_btn.text = getString(R.string.logout_button_message)
+            authenticate_btn.setOnClickListener {
+                LoginApi.client().logout()
+                Toast.makeText(this, "Logged out!", Toast.LENGTH_LONG).show()
+                finish()
+                startActivity(intent)
+            }
         }
         else {
             supportFragmentManager.beginTransaction().replace(R.id.frameLayout, UnauthenticatedFragment()).commit()
-            authenticate_btn.text = getString(R.string.logout_button_message)
+            authenticate_btn.text = getString(R.string.authenticate_button_message)
+            authenticate_btn.setOnClickListener {
+                val authenticateIntent = Intent(this, Authenticate::class.java)
+                startActivity(authenticateIntent)
+            }
         }
     }
 
-    fun authenticate(view: View) {
-        val authenticateIntent = Intent(this, Authenticate::class.java)
-        startActivity(authenticateIntent)
+    fun deleteAccount(view: View) {
+        Toast.makeText(this, "DELETED!", Toast.LENGTH_LONG).show()
+        LoginApi.client().deleteAccount()
+        finish()
+        startActivity(intent)
     }
 }

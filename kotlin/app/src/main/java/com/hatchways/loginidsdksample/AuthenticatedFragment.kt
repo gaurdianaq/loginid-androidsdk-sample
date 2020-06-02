b9479c0071ff54384375f932c6d062a006939a36
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-06-02 19:00:45 -0400

    Now shows username when authenticated.

--- kotlin/app/src/main/java/com/hatchways/loginidsdksample/AuthenticatedFragment.kt
+++ kotlin/app/src/main/java/com/hatchways/loginidsdksample/AuthenticatedFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_authenticated.*
+import login.api.LoginApi
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -17,6 +19,11 @@ class AuthenticatedFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_authenticated, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        authenticatedMsg.text = getString(R.string.authenticated_message, LoginApi.client().currentUsername)
     }
 
 }
